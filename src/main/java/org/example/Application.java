90a2827c22de3345cbd40faf1d8835d03943a750
LuxS94 <simonetti.lucio@libero.it>
2026-01-26 15:10:45 +0100

    configuration

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,15 @@
 package org.example;
 
-public class Application {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("trasporti");
     public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+        em.close();
+        emf.close();
         System.out.println("Hello World!");
     }
 }
