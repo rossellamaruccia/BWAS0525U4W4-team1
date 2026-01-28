07222aea7befe26236498d07649648387d820030
emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2026-01-28 12:54:32 +0100

    [UPD] Creato Abbonamento, aggiunte le relazioni, ManyToOne con emittente, e OneToOne con tessere

--- src/main/java/entities/Abbonamento.java
+++ src/main/java/entities/Abbonamento.java
@@ -4,38 +4,43 @@ import enums.FrequenzaAbbonamento;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "abbonamento")
 public class Abbonamento extends TitoloDiViaggio {
 
-    LocalDate dataScadenza;
-    @Column(name = "frequenza")
-    @Enumerated(EnumType.STRING)
-    private FrequenzaAbbonamento frequenza;
+    @Column(name = "data_scadenza")
+    private LocalDate dataScadenza;
+
+    @Column(name = "durata")
+    @Enumerated(EnumType.ORDINAL)
+    private FrequenzaAbbonamento durata;
+
     @OneToOne
-    @JoinColumn(name = "tessera")
+    @JoinColumn(name = "id_tessera")
     private Tessera tessera;
 
     public Abbonamento() {
     }
 
-    public Abbonamento(Emittente emittente, FrequenzaAbbonamento frequenza, Tessera tessera, int year, int month, int day) {
-        super(year, month, day, emittente);
-        this.frequenza = frequenza;
+    public Abbonamento(LocalDateTime dataEmissione, Emittente emittente, FrequenzaAbbonamento durata, Tessera tessera) {
+        super(dataEmissione, emittente);
+        this.durata = durata;
         this.tessera = tessera;
-        if (frequenza == FrequenzaAbbonamento.SETTIMANALE) {
-            this.dataScadenza = this.getDataEmissione().plusWeeks(1);
-        } else if (frequenza == FrequenzaAbbonamento.MENSILE) {
-            this.dataScadenza = this.getDataEmissione().plusMonths(1);
+        if (this.durata == FrequenzaAbbonamento.SETTIMANALE) {
+            this.dataScadenza = dataEmissione.toLocalDate().plusWeeks(1);
+        } else if (this.durata == FrequenzaAbbonamento.MENSILE) {
+            this.dataScadenza = dataEmissione.toLocalDate().plusMonths(1);
         }
     }
 
-    public FrequenzaAbbonamento getFrequenza() {
-        return frequenza;
+    public FrequenzaAbbonamento getDurata() {
+        return durata;
     }
 
-    public void setFrequenza(FrequenzaAbbonamento frequenza) {
-        this.frequenza = frequenza;
+    public void setDurata(FrequenzaAbbonamento durata) {
+        this.durata = durata;
     }
 
     public Tessera getTessera() {
@@ -48,5 +53,17 @@ public class Abbonamento extends TitoloDiViaggio {
 
     public LocalDate getDataScadenza() {
         return dataScadenza;
+    }
+
+    @Override
+    public String toString() {
+        return "Abbonamento{" +
+                "id=" + getId() +
+                ", dataEmissione=" + getDataEmissione() +
+                ", emittente=" + getEmittente() +
+                ", durata=" + durata +
+                ", dataScadenza=" + dataScadenza +
+                ", tessera=" + tessera +
+                '}';
     }
 }
