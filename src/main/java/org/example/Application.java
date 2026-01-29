77fd1006c480b78f4014309f415c57f8ebeec23c
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2026-01-29 11:03:55 +0100

    empties Application

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,7 +1,6 @@
 package org.example;
 
 
-import dao.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -11,19 +10,6 @@ public class Application {
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
-        UtenteDAO ud = new UtenteDAO(em);
-        TesseraDAO td = new TesseraDAO(em);
-        EmittenteDAO ed = new EmittenteDAO(em);
-        AbbonamentoDAO ad = new AbbonamentoDAO(em);
-        BigliettoDAO bd = new BigliettoDAO(em);
-        Parco_mezziDAO pmd = new Parco_mezziDAO(em);
-        TrattaDAO trattad = new TrattaDAO(em);
-        TitoloDiViaggioDAO tdvd = new TitoloDiViaggioDAO(em);
-
-
-        trattad.trovaPerID("c9b7efb9-0d23-404e-bae3-bd4675253995");
-        pmd.findById("af70e450-f7ca-4bf7-b9bf-723e62a6667b");
-
 
         System.out.println("Hello World!");
 
